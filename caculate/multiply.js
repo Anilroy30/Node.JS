5f4204c3a05e2dca1e4a3a1e4c3b35d5e53164c3
Striver <anilkumarakula34@gmail.com>
2025-02-07 17:47:18 +0530

    modules behind the scenes

--- caculate/multiply.js
+++ caculate/multiply.js
@@ -1,7 +1,14 @@
+require("../xyz.js");
+
 function calculateMultiply(a, b) {
   const result = a * b;
 
   console.log(result);
 }
 
+setTimeout(() => {
+  console.log("setTimout");
+}, 3000);
+
 module.exports = { calculateMultiply };
+
